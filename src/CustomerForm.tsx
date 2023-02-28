5a2e97f7ff352409bd379dafc00be36cc0e97641
Daniel Russom <daniel.russom00@gmail.com>
2023-02-28 12:10:45 +0000

    Add first name input field

--- src/CustomerForm.tsx
+++ src/CustomerForm.tsx
@@ -1,3 +1,6 @@
 import * as React from "react";
 
-export const CustomerForm = () => <form />;
+export const CustomerForm = () => (<form>
+    <input name="firstName"/>
+</form>
+);
--- test/CustomerForm.test.tsx
+++ test/CustomerForm.test.tsx
@@ -14,5 +14,12 @@ describe("CustomerForm", () => {
     it("renders a form", () => {
         render(<CustomerForm />);
         expect(element("form")).not.toBeNull();
-    });
+
+    });it("renders the first name field as a text box", () => {
+        render(<CustomerForm />);
+        const field = element("form").elements.firstName;
+        expect(field).not.toBeNull();
+        expect(field.tagName).toEqual("INPUT");
+        expect(field.type).toEqual("text");
+       });
 })
