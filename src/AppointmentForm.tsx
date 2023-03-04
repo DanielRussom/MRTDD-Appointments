53a907416ee44d1eee33252fc66a0f1b2ee7226e
Daniel Russom <daniel.russom00@gmail.com>
2023-03-04 15:09:02 +0000

    Initialise TimeslotTable

--- src/AppointmentForm.tsx
+++ src/AppointmentForm.tsx
@@ -1,17 +1,20 @@
 import * as React from "react";
 
+const TimeSlotTable = () => <table id="time-slots" />;
 
 export const AppointmentForm = ({ selectableServices, original }) => {
     return (
         <form>
             <select name="service"
                 value={original.service}
+                readOnly
                 >
                 <option />
                 {selectableServices.map(s => (
                     <option key={s}>{s}</option>
                 ))}
             </select>
+            <TimeSlotTable />
         </form>
     )
 }
--- test/AppointmentForm.test.tsx
+++ test/AppointmentForm.test.tsx
@@ -4,6 +4,7 @@ import {
     render,
     field,
     form,
+    element,
 } from "./reactTestExtensions";
 import { AppointmentForm } from "../src/AppointmentForm";
 
@@ -41,38 +42,50 @@ describe("AppointmentForm", () => {
             expect(field("service")).not.toBeNull();
             expect(field("service").tagName).toEqual("SELECT");
         });
+
+        it("renders as a select box", () => {
+            render(<AppointmentForm original={blankAppointment} />);
+            expect(field("service")).not.toBeNull();
+            expect(field("service").tagName).toEqual("SELECT");
+        });
+
+        it("has a blank value as the first value", () => {
+            render(<AppointmentForm original={blankAppointment} />);
+            const firstOption = field("service").childNodes[0];
+            expect(firstOption.value).toEqual("");
+        });
+
+        it("lists all salon services", () => {
+            render(<AppointmentForm selectableServices={services} original={blankAppointment} />);
+
+            expect(labelsOfAllOptions(field("service"))).toEqual(expect.arrayContaining(services));
+        });
+
+        it("pre-selects the existing value", () => {
+            const appointment = { service: "Blow-dry" };
+            render(
+                <AppointmentForm
+                    selectableServices={services}
+                    original={appointment}
+                />
+            );
+            const option: any = findOption(
+                field("service"),
+                "Blow-dry"
+            );
+            expect(option.selected).toBe(true);
+        });
+
     });
-
-    it("renders as a select box", () => {
-        render(<AppointmentForm original={blankAppointment} />);
-        expect(field("service")).not.toBeNull();
-        expect(field("service").tagName).toEqual("SELECT");
-    });
-
-    it("has a blank value as the first value", () => {
-        render(<AppointmentForm original={blankAppointment} />);
-        const firstOption = field("service").childNodes[0];
-        expect(firstOption.value).toEqual("");
-    });
-
-    it("lists all salon services", () => {
-        render(<AppointmentForm selectableServices={services} original={blankAppointment} />);
-
-        expect(labelsOfAllOptions(field("service"))).toEqual(expect.arrayContaining(services));
-    });
-
-    it("pre-selects the existing value", () => {
-        const appointment = { service: "Blow-dry" };
-        render(
-            <AppointmentForm
-                selectableServices={services}
-                original={appointment}
-            />
-        );
-        const option: any = findOption(
-            field("service"),
-            "Blow-dry"
-        );
-        expect(option.selected).toBe(true);
+    describe("time slot table", () => {
+        it("renders a table for time slots with an id", () => {
+            render(
+                <AppointmentForm original={blankAppointment} />
+            );
+            expect(
+                element("table#time-slots")
+            ).not.toBeNull();
+        });
     });
 });
+
